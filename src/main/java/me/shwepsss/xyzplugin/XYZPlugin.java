b27e8c1aecd92b89b77c5a122c6af81193d458cf
yuval <yuval.levy@ld.amalnet.k12.il>
2023-12-28 19:46:52 +0200

    added the ability to see all players current position in player list (tab) it is enabled automatically and there is no way to disable it currently

--- src/main/java/me/shwepsss/xyzplugin/XYZPlugin.java
+++ src/main/java/me/shwepsss/xyzplugin/XYZPlugin.java
@@ -4,13 +4,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class XYZPlugin extends JavaPlugin {
+public final class XYZPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+            getServer().getPluginManager().registerEvents(this, this);
+        }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+    Player p = event.getPlayer();
+    p.setPlayerListName(p.getName()+" "+getPLocation(p));
+
 
     }
 
@@ -20,11 +31,12 @@ public final class XYZPlugin extends JavaPlugin {
             if (sender instanceof Player){
                 Player p = (Player) sender;
                 if (args.length == 0) {
-                    getPLocation(p);
+                    p.chat(getPLocation(p));
 
                 }else if(args.length == 1){
                     Player player = Bukkit.getServer().getPlayer(args[0]);
-                    p.sendMessage(player.getName()+" XYZ Location is: "+Math.round(player.getLocation().getX()) + ", " + Math.round(player.getLocation().getY()) + ", " + Math.round(player.getLocation().getZ()));
+                    p.sendMessage(getPLocation(player));
+
 
 
                 }
@@ -36,16 +48,18 @@ public final class XYZPlugin extends JavaPlugin {
         }
 
 
+
+
+
         return true;
     }
 
-    public void getPLocation(Player p){
-        p.chat(p.getName()+" XYZ Location is: "+Math.round(p.getLocation().getX()) + ", " + Math.round(p.getLocation().getY()) + ", " + Math.round(p.getLocation().getZ()));
+    public String getPLocation(Player p){
+        return Math.round(p.getLocation().getX()) + ", " + Math.round(p.getLocation().getY()) + ", " + Math.round(p.getLocation().getZ());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
 
     }
 }
