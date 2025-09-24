630aab93a47981baff2c2f21bbe3431c45a6b5ef
yuval <yuvallevy2004@gmail.com>
2025-09-24 16:34:46 +0300

    added more functions
    add, list, delete
    
    allows user to save,list,delete waypoints in sever database by user uuid
    
    also when the player dies it sends its death XYZ to himself to allow him to go grab his inventory items
    
    and when a user leaves also writes in chat its quit XYZ coordinates in case he is lesner and falls into lava in the nether

--- src/main/java/me/shwepsss/xyzplugin/XYZPlugin.java
+++ src/main/java/me/shwepsss/xyzplugin/XYZPlugin.java
@@ -3,17 +3,21 @@ package me.shwepsss.xyzplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class XYZPlugin extends JavaPlugin implements Listener {
+import java.io.File;
+import java.io.IOException;
 
+public final class XYZPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-
             getServer().getPluginManager().registerEvents(this, this);
         }
 
@@ -21,8 +25,18 @@ public final class XYZPlugin extends JavaPlugin implements Listener {
     public void onPlayerMove(PlayerMoveEvent event){
     Player p = event.getPlayer();
     p.setPlayerListName(p.getName()+" "+getPLocation(p));
+    }
 
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+        Player p = event.getPlayer();
+        Bukkit.broadcastMessage(p.getName()+" left the server at the XYZ coordinates: " + getPLocation(p));
+    }
 
+    @EventHandler
+    public void onPlayerDie(PlayerDeathEvent event){
+        Player p = event.getEntity().getPlayer();
+        p.sendMessage("You died at the XYZ coordinates: " + getPLocation(p));
     }
 
     @Override
@@ -32,28 +46,62 @@ public final class XYZPlugin extends JavaPlugin implements Listener {
                 Player p = (Player) sender;
                 if (args.length == 0) {
                     p.chat(getPLocation(p));
+                }
+                else if(args.length == 2 && args[0].equals("add")){
 
-                }else if(args.length == 1){
+                    File playerFile = new File(getDataFolder() + "/playerdata", p.getUniqueId() + ".yml");
+                    YamlConfiguration data = YamlConfiguration.loadConfiguration(playerFile);
+                    data.set(args[1], getPLocation(p));
+                    saveData(data, playerFile);
+                }
+                else if(args[0].equals("list")){
+                    File playerFile = new File(getDataFolder() + "/playerdata", p.getUniqueId() + ".yml");
+                    YamlConfiguration data = YamlConfiguration.loadConfiguration(playerFile);
+                    if (args.length == 1) {
+                        for (String key : data.getKeys(false)) {
+                            Object value = data.get(key);
+                            p.sendMessage(key + ": " + value);
+                        }
+                    }else if(args.length == 2){
+                        p.sendMessage(args[1]+ ": " + data.get(args[1]));
+                    }
+                }
+
+                else if(args.length == 2 && args[0].equals("delete")){
+                    File playerFile = new File(getDataFolder() + "/playerdata", p.getUniqueId() + ".yml");
+                    YamlConfiguration data = YamlConfiguration.loadConfiguration(playerFile);
+                    if (args[1].equals("all")){
+                        for (String key : data.getKeys(false)) {
+                            data.set(key, null);
+                            saveData(data, playerFile);
+                        }
+                    }
+                        else{
+                        for (String key : data.getKeys(false)) {
+                            if (key.equals(args[1])){
+                                data.set(key, null);
+                                saveData(data, playerFile);
+                            }
+                        }
+                    }
+                }
+                else if(args.length == 1){
                     Player player = Bukkit.getServer().getPlayer(args[0]);
                     p.sendMessage(getPLocation(player));
 
-
-
                 }
             }
-
-
-
-
         }
-
-
-
-
-
         return true;
     }
+    void saveData(YamlConfiguration data, File playerFile){
+        try {
+            data.save(playerFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+    }
     public String getPLocation(Player p){
         return Math.round(p.getLocation().getX()) + ", " + Math.round(p.getLocation().getY()) + ", " + Math.round(p.getLocation().getZ());
     }
